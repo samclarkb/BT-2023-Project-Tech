45d02b30759df9d452af460c5b6b8b7c8da548bf
samclarkb <74268703+samclarkb@users.noreply.github.com>
2023-02-09 14:48:33 +0100

    created some standard routes

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,25 @@ const express = require('express')
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT
+const student = require('./student.js')
 
 app.get('/', (req, res) => {
+	console.log('student', student)
+	res.send('Home page')
+})
+
+app.get('/about', (req, res) => {
 	console.log('hello world!')
-	res.send('hello world!')
+	res.send('About page')
+})
+
+app.get('/login', (req, res) => {
+	console.log('login screen')
+	res.send('login screen')
+})
+
+app.get('*', function (req, res) {
+	res.status(404).send('404 page')
 })
 
 app.listen(port, () => {
